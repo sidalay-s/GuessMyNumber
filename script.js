852c607b63f66114ee2231cf9c9375cb3e2a4e68
Sid Shingaki <sidalaytv@gmail.com>
2022-05-31 01:26:53 -0700

    feat: add messageText() function
    - added function to easily change display message
    - added first if check block

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ let score = 20;
 let highscore = 0;
 let won = false;
 
+const messageText = function(message) {
+    document.querySelector(".message").textContent = message;
+} 
+
 document.querySelector(".check").addEventListener("click", function() {
+    const guess = Number(document.querySelector(".guess").textContent);
     
+    if (!won) {
+        if (!guess) {
+            messageText("No Number!");
+        }
+    }
 })
