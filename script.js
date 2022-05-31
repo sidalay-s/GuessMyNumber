0d948f7a815313e0002a71fc7eed3e4abe4a93e6
Sid Shingaki <sidalaytv@gmail.com>
2022-05-31 02:17:34 -0700

    feat: Added reset function
    - game can now be reset while maintaining high score

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ const grabElement = function(element) {
     return document.querySelector(element);
 }
 
-document.querySelector(".check").addEventListener("click", function() {
-    const guess = Number(document.querySelector(".guess").value);
+grabElement(".check").addEventListener("click", function() {
+    const guess = Number(grabElement(".guess").value);
     
     // if user has not won
     if (!won) {
@@ -27,6 +27,7 @@ document.querySelector(".check").addEventListener("click", function() {
             won = true;
             displayText(".message", "Correct number!");
             displayText(".number", secretNumber);
+            displayText(".title", "You Guessed It!");
             grabElement(".message").style.color = "#60b347";
             grabElement(".number").style.backgroundColor = "#60b347";
             grabElement(".number").style.width = "30rem";
@@ -44,6 +45,7 @@ document.querySelector(".check").addEventListener("click", function() {
                 displayText(".score", score);
             }
             else {
+                displayText(".title", "The Number Was");
                 displayText(".message", "You have lost!");
                 displayText(".score", 0);
                 displayText(".number", secretNumber);
@@ -53,5 +55,20 @@ document.querySelector(".check").addEventListener("click", function() {
             }
         }
     }
+})
 
+grabElement(".again").addEventListener("click", function() {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    won = false;
+    score = 20;
+    displayText(".message", "Start guessing...");
+    displayText(".score", score);
+    displayText(".number", "?");
+    displayText(".title", "Guess My Number!");
+    grabElement(".guess").value = "";
+    grabElement(".number").style.backgroundColor = "#eee";
+    grabElement(".number").style.color = "#222";
+    grabElement(".number").style.width = "15rem";
+    grabElement(".message").style.color = "#eee";
+    grabElement(".highscore").style.color = "#eee";
 })
